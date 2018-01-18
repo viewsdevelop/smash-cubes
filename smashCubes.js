cc7afc75b5a90f6bc5fe3ca3e132df803ccb25d6
mosi.rivera <mosileko@yahoo.com>
2018-01-18 16:15:35 -0500

    Added diagonal end vertical projectile directions. Still bug on dSpecial down  arrow keyup that executes nSpecial if W key is still pressed.

--- smashCubes.js
+++ smashCubes.js
@@ -1,20 +1,36 @@
 var activeMap;
 var activeCharacter;
-var projectiles = {"neg":[],"pos":[]};
+//stores projectiles, keys corespond to cuadrants with the character that creates them being (0,0)
+var projectiles = {
+  "(0,-)":[],
+  "(0,+)":[],
+  "(-,0)":[],
+  "(+,0)":[],
+  "(+,+)":[],
+  "(-,+)":[],
+  "(-,-)":[],
+  "(+,-)":[]
+};
 
 function startGame(){
+  //characters
   melee = new component(30,30,"green",300,300);
-  ranged = new component(30,30,"blue",300,300);
-  playerDamage = new component("38px","consolas","white",window.innerWidth / 3.5,window.innerHeight - 100, "text");
+  ranged = new component(30,40,"blue",300,300);
+  sowrdsman = new component();
+  //text that displays the pleyers damage taken
+  playerOneDamage = new component("38px","consolas","white",window.innerWidth / 3.5,window.innerHeight - 100, "text");
+  playerTwoDamage = new component("38px","consolas","white",window.innerWidth / 1.5,window.innerHeight - 100,"text");
+  //blue square used when melee character is using dSpecial
   chargingAnimation = new component(50,50,"blue",0,0)
+  //training object
   punchingBag = new component(30,60,"purple",(window.innerWidth / 2) - 15,300);
-  punchingBagDamage = new component("38px","consolas","white",window.innerWidth / 1.5,window.innerHeight - 100,"text");
+  //stage and platforms
   stage = new component(window.innerWidth / 1.8,10,"black",(window.innerWidth - window.innerWidth / 1.8) / 2,500);
   platformOne = new component();
   platformTwo = new component();
   myGameArea.start();
 }
-
+//mygame area, start, clear, and stop functions.
 var myGameArea = {
   canvas : document.createElement("canvas"),
   start : function(){
@@ -33,13 +49,14 @@ var myGameArea = {
   }
 };
 
+
 function everyInterval(n){
   if ((myGameArea.frameNo / n) % 1 === 0) {
     return true
   }
   return false
 };
-
+//create and manipulate components
 function component(width,height,color,x,y,type){
   this.type = type;
   if (type == "image") {
@@ -54,7 +71,6 @@ function component(width,height,color,x,y,type){
   this.y = y;
   this.gravity = 1;
   this.gravitySpeed = 0;
-  this.damage = 0;
   this.jumpCount = 0;
   this.damage = 0;
   this.charge = 0;
@@ -62,20 +78,24 @@ function component(width,height,color,x,y,type){
   this.canMoveLeft = true;
   this.canMoveRight = true;
   this.canAttack = true;
+  this.canSpecial = true;
+  this.hitStun = false;
   this.upPressed = false;
   this.downPressed = false;
+  //attacks depending on arrows pressed; none: nAttack, up: upAttack, down: dAttack, right: fAttack, left: bAttack;
   this.nAttack = false;
   this.uAttack = false;
   this.dAttack = false;
   this.fAttack = false;
   this.bAttack = false;
-  this.canSpecial = true;
+  //same as above but with special attacks
   this.nSpecial = false;
   this.uSpecial = false;
   this.dSpecial = false;
   this.fSpecial = false;
   this.bSpecial = false;
-  this.update = function() {
+
+  this.update = function() { //updates depending on type
       ctx = myGameArea.context;
       if (type == "image") {
           ctx.drawImage(this.image,
@@ -92,15 +112,14 @@ function component(width,height,color,x,y,type){
       };
   };
 
-  this.newPos = function() {
-
+  this.newPos = function() { //applies gravity and speeds to positions
     this.gravitySpeed += this.gravity;
     this.x += this.speedX;
     this.y += this.speedY + this.gravitySpeed;
     this.hitBottom();
   };
 
-  this.crashWith = function(otherobj){
+  this.crashWith = function(otherobj){ //if this comes in contact with otherobj returns true else false
     var myleft = this.x;
     var myright = this.x + (this.width);
     var mytop = this.y;
@@ -118,7 +137,7 @@ function component(width,height,color,x,y,type){
     return crash
   };
 
-  this.didHit = function(otherobj){
+  this.didHit = function(otherobj){ //same as above but while attacking and applies damage
     var myleft = this.x;
     var myright = this.x + (this.width);
     var mytop = this.y;
@@ -131,7 +150,12 @@ function component(width,height,color,x,y,type){
     if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
       hit = false;
     } else {
-      if (projectiles["neg"].includes(this) || projectiles["pos"].includes(this)) {
+      if (projectiles["(-,0)"].includes(this) ||
+          projectiles["(+,0)"].includes(this) ||
+          projectiles["(+,+)"].includes(this) ||
+          projectiles["(-,+)"].includes(this) ||
+          projectiles["(-,-)"].includes(this) ||
+          projectiles["(+,-)"].includes(this)) {
         otherobj.damage += 0.2;
         otherobj.gravitySpeed = 0;
       } else {
@@ -145,11 +169,12 @@ function component(width,height,color,x,y,type){
         otherobj.speedX -= otherobj.damage;
         otherobj.speedY -= otherobj.damage * 2;
       }
+      otherobj.hitStun = true;
     }
   }
 
   this.hitBottom = function(){
-    //make it so it falls of stagge if not over stage
+    //make it so it falls of stage if not over it
     var rockbottom = myGameArea.canvas.height;
     if (this.crashWith(stage)){
       rockbottom = myGameArea.canvas.height - this.height - (myGameArea.canvas.height - stage.y);
@@ -218,7 +243,7 @@ function component(width,height,color,x,y,type){
      this.downPressed = false;
    };
 
-  this.attack = function() {
+  this.attack = function() { //adds to this's size to make it attack deending on the key pressed;
     if (this.canAttack) {
       if (this.speedX == 5) {
         this.width += 20;
@@ -244,7 +269,7 @@ function component(width,height,color,x,y,type){
     this.didHit(punchingBag);
   };
 
-  this.clearAttack = function(){
+  this.clearAttack = function(){ //clears the change in this's size from the attack function
 
     if (this.fAttack) {
       this.width -= 20;
@@ -270,19 +295,19 @@ function component(width,height,color,x,y,type){
 
   this.specialAttack = function(){
     if(this.canSpecial){
-      if (this.speedX == 5) {
+      if (this.speedX == 5) { //if right key pressed, create projectile towards right: HADOUKEN!
         if (this.charge >= 10 ) {
-          projectiles["pos"].push(new component(this.charge,this.charge,"blue",this.x,this.y + (this.height / 3)));
-          this.charge -= 10;
+          projectiles["(+,0)"].push(new component(this.charge,this.charge,"blue",this.x,this.y + (this.height / 3)));// size depends on charge
+          this.charge -= 10; // reduces charge after using
         }
-        this.fSpecial = true;
+        this.fSpecial = true; // same as above but to the left: HADOUKEN!
       } else if (this.speedX == -5) {
         if (this.charge >= 10 ) {
-          projectiles["neg"].push(new component(this.charge,this.charge,"blue",this.x + (this.width),this.y + (this.height / 3)));
+          projectiles["(-,0)"].push(new component(this.charge,this.charge,"blue",this.x + (this.width),this.y + (this.height / 3)));
           this.charge -= 10;
         }
         this.bSpecial = true
-      } else if (this.upPressed) {
+      } else if (this.upPressed) { //upwards jump that strikes twice at beggining of animation
         this.gravitySpeed = -20;
         this.attack();
         this.attack();
@@ -290,22 +315,117 @@ function component(width,height,color,x,y,type){
         this.uSpecial = true;
         this.jumpCount = 2;
         this.canSpecial = false;
-      } else if (this.downPressed) {
+      } else if (this.downPressed) { //adds to the charge variable until charge is 55
         if (this.charge <= 50) {
           this.charge += 5;
           this.charging = true;
         }
+
         this.dSpecial = true;
-      }
+      } else if (this.charge > 50){ // neutral special, makes charge 0: too OP // not sure if let the projectile go through stage or dissapear on contact
+        let projectileSize = 25;
+        projectiles["(0,+)"].push(new component(projectileSize, projectileSize,"black",this.x + (this.width / 2),this.y + (this.height / 2)));
+        projectiles["(+,0)"].push(new component(projectileSize, projectileSize,"black",this.x + (this.width / 2),this.y + (this.height / 2)));
+        projectiles["(-,0)"].push(new component(projectileSize, projectileSize,"black",this.x + (this.width / 2),this.y + (this.height / 2)));
+        projectiles["(+,+)"].push(new component(projectileSize, projectileSize,"black",this.x + (this.width / 2),this.y + (this.height / 2)));
+        projectiles["(-,+)"].push(new component(projectileSize, projectileSize,"black",this.x + (this.width / 2),this.y + (this.height / 2)));
+        projectiles["(-,-)"].push(new component(projectileSize, projectileSize,"black",this.x + (this.width / 2),this.y + (this.height / 2)));
+        projectiles["(+,-)"].push(new component(projectileSize, projectileSize,"black",this.x + (this.width / 2),this.y + (this.height / 2)));
+        this.charge = 0;
+        }
+        this.nSpecial = true;
     };
   }
 
   this.clearSpecial = function(){
     if (this.dSpecial) {
-      this.charging = false
+      this.charging = false;
+
     };
   };
 };
+
+function updateProjectiles(){ //updates projectiles in the proper direction
+  let verticalSpeed = 10;
+  let horizontalSpeed = 10;
+  let diagonalSpeed = 7.5;
+  for (var i = 0; i < projectiles["(0,+)"].length; i++) {
+    let projectile = projectiles["(0,+)"][i];
+    projectile.gravity = 0;
+    projectile.speedY = -verticalSpeed;
+    projectile.newPos();
+    projectile.update();
+    projectile.didHit(punchingBag);
+  }
+  for (var i = 0; i < projectiles["(0,-)"].length; i++) {
+    let projectile = projectiles["(0,-)"][i];
+    projectile.gravity = 0;
+    projectile.speedX = verticalSpeed;
+    projectile.newPos();
+    projectile.update();
+    projectile.didHit(punchingBag);
+  }
+  for (var i = 0; i < projectiles["(+,0)"].length; i++) {
+    let projectile = projectiles["(+,0)"][i];
+    projectile.gravity = 0;
+    projectile.speedX = horizontalSpeed;
+    projectile.newPos();
+    projectile.update();
+    projectile.didHit(punchingBag);
+  }
+  for (var i = 0; i < projectiles["(-,0)"].length; i++) {
+    let projectile = projectiles["(-,0)"][i];
+    projectile.gravity = 0;
+    projectile.speedX = -horizontalSpeed;
+    projectile.newPos();
+    projectile.update();
+    projectile.didHit(punchingBag);
+  }
+  for (var i = 0; i < projectiles["(+,+)"].length; i++) {
+    let projectile = projectiles["(+,+)"][i];
+    projectile.gravity = 0;
+    projectile.speedX = diagonalSpeed;
+    projectile.speedY = diagonalSpeed;
+    projectile.newPos();
+    projectile.update();
+    projectile.didHit(punchingBag);
+  }
+  for (var i = 0; i < projectiles["(-,+)"].length; i++) {
+    let projectile = projectiles["(-,+)"][i];
+    projectile.gravity = 0;
+    projectile.speedX = -diagonalSpeed;
+    projectile.speedY = diagonalSpeed;
+    projectile.newPos();
+    projectile.update();
+    projectile.didHit(punchingBag);
+  }
+  for (var i = 0; i < projectiles["(-,-)"].length; i++) {
+    let projectile = projectiles["(-,-)"][i];
+    projectile.gravity = 0;
+    projectile.speedX = -diagonalSpeed;
+    projectile.speedY = -diagonalSpeed;
+    projectile.newPos();
+    projectile.update();
+    projectile.didHit(punchingBag);
+  }
+  for (var i = 0; i < projectiles["(+,-)"].length; i++) {
+    let projectile = projectiles["(+,-)"][i];
+    projectile.gravity = 0;
+    projectile.speedX = diagonalSpeed;
+    projectile.speedY = -diagonalSpeed;
+    projectile.newPos();
+    projectile.update();
+    projectile.didHit(punchingBag);
+  }
+}
+
+function updateChargeAnimation(character){
+  if (character.charging && character.charge < 55) {
+    chargingAnimation.x = character.x - 10;
+    chargingAnimation.y = character.y - 10;
+    chargingAnimation.update()
+  }
+}
 
 
 
@@ -315,31 +435,12 @@ function updateGameArea(){
   if (punchingBag.crashWith(stage)) {
     punchingBag.clearSpeeds();
   }
-  if (melee.charging && melee.charge < 50) {
-    chargingAnimation.x = melee.x - 10;
-    chargingAnimation.y = melee.y - 10;
-    chargingAnimation.update()
-  }
-  for (var i = 0; i < projectiles["pos"].length; i++) {
-    let projectile = projectiles["pos"][i];
-    projectile.gravity = 0;
-    projectile.speedX = 10;
-    projectile.newPos();
-    projectile.update();
-    projectile.didHit(punchingBag);
-  }
-  for (var i = 0; i < projectiles["neg"].length; i++) {
-    let projectile = projectiles["neg"][i];
-    projectile.gravity = 0;
-    projectile.speedX = -10;
-    projectile.newPos();
-    projectile.update();
-    projectile.didHit(punchingBag);
-  }
-  playerDamage.text = Math.round(melee.damage * 10) + "%";
-  playerDamage.update();
-  punchingBagDamage.text = Math.round(punchingBag.damage * 10) + "%";
-  punchingBagDamage.update();
+  updateChargeAnimation(melee);
+  updateProjectiles();
+  playerOneDamage.text = Math.round(melee.damage * 10) + "%";
+  playerOneDamage.update();
+  playerTwoDamage.text = Math.round(punchingBag.damage * 10) + "%";
+  playerTwoDamage.update();
   punchingBag.update();
   melee.newPos();
   melee.update();
